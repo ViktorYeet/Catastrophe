43247faf4be76cde3f3c6deb8e8f14629c3d489e
Spiritfire0 <hansson.filip02@gmail.com>
2025-02-18 14:05:38 +0100

    Wrote The script

--- My project/Assets/Scripts/PushObj.cs
+++ My project/Assets/Scripts/PushObj.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class PushObj : MonoBehaviour
 {
+
+    private float pushPower = 4;
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        //check if object did not have rigid body or is Kinematic
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        if(hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        //
+        Vector3 collisionPoint = hit.point;
+        body.AddForceAtPosition(pushDir * pushPower, collisionPoint, ForceMode.Impulse);
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {

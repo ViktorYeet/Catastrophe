a1c3990a49423d33309f4bf49f4b7d44eaa02b59
Spiritfire0 <hansson.filip02@gmail.com>
2025-02-20 15:08:36 +0100

    Started writing the script

---
+++ My project/Assets/Scripts/PickupObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    [SerializeField] private Transform PlayerCameraTransform;
+    [SerializeField] private LayerMask pickUpLayerMask;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float pickupdistance = 2f;
+            if (Physics.Raycast(PlayerCameraTransform.position, PlayerCameraTransform.forward, out RaycastHit raycasthit, pickupdistance)) // ? Added missing parenthesis
+            {
+                Debug.Log(raycasthit.transform);
+                if (raycasthit.transform.TryGetComponent<ObjectGrabbable>(out ObjectGrabbable objectGrabbable)) // ? Fixed incorrect 'out' syntax
+                {
+                    Debug.Log(objectGrabbable);
+                }
+            }
+        }
+    }
+}

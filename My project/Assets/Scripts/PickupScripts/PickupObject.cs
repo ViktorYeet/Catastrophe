76314cd3af230dc6c841447b8b836f5eb22722c6
Spiritfire0 <hansson.filip02@gmail.com>
2025-02-20 15:19:34 +0100

    Added a ObjectGrabbable Script

--- My project/Assets/Scripts/PickupScripts/PickupObject.cs
+++ My project/Assets/Scripts/PickupScripts/PickupObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PickupObject : MonoBehaviour
 {
     [SerializeField] private Transform PlayerCameraTransform;
+    [SerializeField] private Transform objectGrabPointTransform;
     [SerializeField] private LayerMask pickUpLayerMask;
 
     private void Update()
@@ -15,7 +16,7 @@ public class PickupObject : MonoBehaviour
             if (Physics.Raycast(PlayerCameraTransform.position, PlayerCameraTransform.forward, out RaycastHit raycasthit, pickupdistance)) // ? Added missing parenthesis
             {
                 Debug.Log(raycasthit.transform);
-                if (raycasthit.transform.TryGetComponent<ObjectGrabbable>(out ObjectGrabbable objectGrabbable)) // ? Fixed incorrect 'out' syntax
+                if (raycasthit.transform.TryGetComponent(out ObjectGrabbable objectGrabbable)) // ? Fixed incorrect 'out' syntax
                 {
                     Debug.Log(objectGrabbable);
                 }

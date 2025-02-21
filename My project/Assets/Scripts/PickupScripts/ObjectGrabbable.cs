24d8ef4be0edc638cdfc6c691f5c58a1ec33facb
Spiritfire0 <hansson.filip02@gmail.com>
2025-02-21 13:59:52 +0100

    Added comments

--- My project/Assets/Scripts/PickupScripts/ObjectGrabbable.cs
+++ My project/Assets/Scripts/PickupScripts/ObjectGrabbable.cs
@@ -7,15 +7,19 @@ public class ObjectGrabbable : MonoBehaviour
 {
     private Rigidbody objectRigidbody;
     private Transform objectGrabPointTransform;
+
+    // Makes it so it only works for Rigidbody
     private void Awake()
     {
         objectRigidbody = GetComponent<Rigidbody>();
     }
+    // Moves the object to the GrabpointTransforms
     public void Grab(Transform objectGrabPointTransform)
     {
         this.objectGrabPointTransform = objectGrabPointTransform;
     }
 
+    //Makes it so it moves with the GrabPointTransform
     private void FixedUpdate()
     {
         if (objectGrabPointTransform != null) {

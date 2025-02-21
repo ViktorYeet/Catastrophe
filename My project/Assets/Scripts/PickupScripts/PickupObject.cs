cae736175dfd855c3f3f29628deaaff55426dcd5
Spiritfire0 <hansson.filip02@gmail.com>
2025-02-21 14:03:25 +0100

    Added even more comments

--- My project/Assets/Scripts/PickupScripts/PickupObject.cs
+++ My project/Assets/Scripts/PickupScripts/PickupObject.cs
@@ -8,14 +8,19 @@ public class PickupObject : MonoBehaviour
     [SerializeField] private Transform objectGrabPointTransform;
     [SerializeField] private LayerMask pickUpLayerMask;
 
+
+    // updates constantly
     private void Update()
     {
+        //Detects if E is pressed down
         if (Input.GetKeyDown(KeyCode.E))
         {
+            //Checks if player is in range of pickup
             float pickupdistance = 2f;
             if (Physics.Raycast(PlayerCameraTransform.position, PlayerCameraTransform.forward, out RaycastHit raycasthit, pickupdistance)) 
             {
                 Debug.Log(raycasthit.transform);
+                //If object has the ObjectGrabbable script grab it, and turn of collision and gravity
                 if (raycasthit.transform.TryGetComponent(out ObjectGrabbable objectGrabbable))
                     objectGrabbable.GetComponent<CapsuleCollider>().enabled = false;
                     objectGrabbable.GetComponent<Rigidbody>().useGravity = false;

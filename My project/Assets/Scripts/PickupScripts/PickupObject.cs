3dae84db37dbfe31d085c733c57fd226c685971a
viktoryeet <viktor@stefansbo.se>
2025-02-24 16:14:03 +0100

    add unity fields for keys and restructured the code to be readable

--- My project/Assets/Scripts/PickupScripts/PickupObject.cs
+++ My project/Assets/Scripts/PickupScripts/PickupObject.cs
@@ -9,41 +9,59 @@ public class PickupObject : MonoBehaviour
     [SerializeField] private Transform objectGrabPointTransform;
     [SerializeField] private LayerMask pickUpLayerMask;
     private ObjectGrabbable objectGrabbable;
+    public KeyCode pickupKey;
+    public KeyCode dropKey;
+    public KeyCode throwKey;
     private float throwPower = 5f;
 
 
     // updates constantly
     private void Update()
     {
-        //Detects if E is pressed down
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Mouse0))
+        // dont do anything unless we pressed one of the keys
+        if( !(Input.GetKeyDown(pickupKey) || Input.GetKeyDown(throwKey) || Input.GetKeyDown(dropKey))) return;
+        
+        //Detects if pickup key is pressed down
+        if (Input.GetKeyDown(pickupKey))
         {
-            if (objectGrabbable == null){
-
-
-                //Checks if player is in range of pickup
-                float pickupdistance = 2f;
-                if (Physics.Raycast(PlayerCameraTransform.position, PlayerCameraTransform.forward, out RaycastHit raycasthit, pickupdistance))
-                {
-                    Debug.Log(raycasthit.transform);
-                    //If object has the ObjectGrabbable script grab it, and turn of collision and gravity
-                    if (raycasthit.transform.TryGetComponent(out objectGrabbable))
-                    {
-                        objectGrabbable.Grab(objectGrabPointTransform);
-                    }
-                }
-            }
-            else if(Input.GetKeyDown(KeyCode.Mouse0)) {
-                //Throw item if something is carried
-                Vector3 throwDirection = PlayerCameraTransform.forward;
-                objectGrabbable.Throw(throwDirection, throwPower);
-                objectGrabbable = null;
-            }
-            else
+            //if we were not holding anything before
+            if (objectGrabbable == null)
             {
-                //Drop item if something is carried
-                objectGrabbable.Drop();
-                objectGrabbable = null;
+                pickup();
+                return;
+            }
+        }
+        if(Input.GetKeyDown(throwKey))
+        {
+            //Throw item if something is carried
+            throwObject();
+        }
+        else
+        {
+            //Drop item if something is carried
+            objectGrabbable.Drop();
+            objectGrabbable = null;
+        }
+    }
+
+    private void throwObject()
+    {
+        Vector3 throwDirection = PlayerCameraTransform.forward;
+        objectGrabbable.Throw(throwDirection, throwPower);
+        objectGrabbable = null;
+    }
+
+    private void pickup()
+    {
+        //Checks if player is in range of pickup
+        float pickupdistance = 2f;
+        if (Physics.Raycast(PlayerCameraTransform.position, PlayerCameraTransform.forward, out RaycastHit raycasthit, pickupdistance))
+        {
+            Debug.Log(raycasthit.transform);
+            //If object has the ObjectGrabbable script grab it, and turn of collision and gravity
+            if (raycasthit.transform.TryGetComponent(out objectGrabbable))
+            {
+                objectGrabbable.Grab(objectGrabPointTransform);
             }
         }
     }

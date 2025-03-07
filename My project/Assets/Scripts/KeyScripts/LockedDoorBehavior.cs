afc1256cb903e479ae39edcb476109464072fec0
viktoryeet <viktor@stefansbo.se>
2025-03-07 13:43:12 +0100

    made the grabbable and changed how locks work

--- My project/Assets/Scripts/KeyScripts/LockedDoorBehavior.cs
+++ My project/Assets/Scripts/KeyScripts/LockedDoorBehavior.cs
@@ -1,20 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using SojaExiles;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DoorBehavior : opencloseDoor
 {
     // Start is called before the first frame update
     public bool locked; 
-    [SerializeField] InventoryManager.AvailableItems requiredItem;
-    protected void OnMouseOver() {
-        if(!locked || InventoryManager.Instance._inventoryItems.Contains(requiredItem)) {
-            base.OnMouseOver();
+    public GameObject requiredObject;
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(String.Format("something moved inside the zone: {0}", other.tag));
+        if(other.gameObject.CompareTag(requiredObject.tag)) {
+            locked = false;
+            Destroy(other.gameObject);
         }
-        else if (Input.GetMouseButtonDown(0)){
-            print("the door is locked");
-        }
+    }
+
+    protected void OnMouseOver()
+    {
+        if (locked) return;
+        base.OnMouseOver();
     }
 }

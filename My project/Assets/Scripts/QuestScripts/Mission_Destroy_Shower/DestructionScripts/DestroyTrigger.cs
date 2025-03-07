c68e1f475a09ae335de8c26f2cdaeca25cba9a66
Erik Fogelstrand <erikfog@chalmers.se>
2025-03-07 11:30:15 +0100

    Default apartment nästan färdig

---
+++ My project/Assets/Scripts/QuestScripts/Mission_Destroy_Shower/DestructionScripts/DestroyTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DestroyTrigger : MonoBehaviour
+{
+    // This is a skeleton that can be used for whatever we would like in the future!
+    public GameObject triggeringObject;
+    public GameObject originalObject;
+    public GameObject destroyedObject;
+    void OnTriggerEnter(Collider Other)
+    {
+        if(Other.tag == triggeringObject.tag)
+        {
+            originalObject.SetActive(false);
+            destroyedObject.SetActive(true);
+        }
+    }
+}

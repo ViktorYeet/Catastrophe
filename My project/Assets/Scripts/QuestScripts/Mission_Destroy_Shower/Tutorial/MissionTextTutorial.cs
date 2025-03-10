e86550181bdbff23f7c486b98a37a9125e4dd607
Spiritfire0 <hansson.filip02@gmail.com>
2025-03-10 14:30:50 +0100

    Added mission text. Not finished

--- My project/Assets/Scripts/QuestScripts/Mission_Destroy_Shower/Tutorial/MissionTextTutorial.cs
+++ My project/Assets/Scripts/QuestScripts/Mission_Destroy_Shower/Tutorial/MissionTextTutorial.cs
@@ -25,9 +25,9 @@ public class MissionTextTutoral: MonoBehaviour
     // Coroutine to change the text after a delay
     IEnumerator UpdateMissionText()
     {
-        Text.text = "Even as a child Dave tried to lock me in";
+        Text.text = "POG";
         yield return new WaitForSeconds(5); // Wait for 5 seconds
-        Text.text = "Move around with ASDW";
+        Text.text = "Try to throw something at the big glass vase.";
     }
 
     void UpdateWineBottlesText()

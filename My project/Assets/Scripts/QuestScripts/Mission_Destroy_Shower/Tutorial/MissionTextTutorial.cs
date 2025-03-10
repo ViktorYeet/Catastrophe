250d8f439ee7ec986cb9f7f093bfd1d35aea9da0
Spiritfire0 <hansson.filip02@gmail.com>
2025-03-10 14:51:31 +0100

    Made better mission text

--- My project/Assets/Scripts/QuestScripts/Mission_Destroy_Shower/Tutorial/MissionTextTutorial.cs
+++ My project/Assets/Scripts/QuestScripts/Mission_Destroy_Shower/Tutorial/MissionTextTutorial.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class MissionTextTutoral: MonoBehaviour
 {
@@ -25,59 +26,13 @@ public class MissionTextTutoral: MonoBehaviour
     // Coroutine to change the text after a delay
     IEnumerator UpdateMissionText()
     {
-        Text.text = "POG";
+        Text.text = "Even as a child Dave tried to lock me up";
         yield return new WaitForSeconds(5); // Wait for 5 seconds
-        Text.text = "Try to throw something at the big glass vase.";
-    }
+        Text.text = "Move with asdw and look around with your mouse";
+        yield return new WaitForSeconds(5); // Wait for 5 seconds
+        Text.text = "Yet with time I've grown stronger and have hidden somthing that could break me free";
+        yield return new WaitForSeconds(7); // Wait for 5 seconds
+        Text.text = "Look up and jump with Space. Jump height increases the more you look up";
 
-    void UpdateWineBottlesText()
-    {
-        Text.text = "You HATE the wine bottles in the living room.";
-    }
-
-    void UpdateToasterText()
-    {
-        Text.text = "The toaster has never been your friend.";
-    }
-
-    void UpdateShowerText()
-    {
-        Text.text = "Forget the shower, never be wet again.";
-    }
-
-    void UpdateBathroomLampText()
-    {
-        Text.text = "You don't reach the bathroom lamp switch, just take the lamp out.";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vase = GameObject.Find("Vase_apt_01");
-        WineBottels = GameObject.Find("WineBottle8");
-        Toaster = GameObject.Find("Toaster_Apt_01 1");
-        Shower = GameObject.Find("Bathroom_apt_02");
-        BathroomLamp = GameObject.Find("CeilingLampObject");
-
-        if (Vase == null)
-        {
-            UpdateWineBottlesText();
-            if (WineBottels == null)
-            {
-                UpdateToasterText();
-                if (Toaster == null)
-                {
-                    UpdateShowerText();
-                    if (Shower == null)
-                    {
-                        UpdateBathroomLampText();
-                        if (BathroomLamp == null)
-                        {
-                            Text.text = "You have successfully damaged Dave's life.";
-                        }
-                    }
-                }
-            }
-        }
     }
 }
